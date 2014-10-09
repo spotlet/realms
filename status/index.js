90c0ecfcbff3ebed6e16e275d317bb4dad622415
Joseph Werle <joseph.werle@gmail.com>
2014-10-09 14:16:08 -0400

    Add status

---
+++ status/index.js
@@ -0,0 +1,34 @@
+
+/**
+ * Module dependencies
+ */
+
+var exec = require('child_process').exec
+  , path = require('path')
+
+var SPOTLET_INDEX = process.env.SPOTLET_INDEX || '/usr/local/spotlets';
+
+module.exports = function (args, socket) {
+  if ('-h' == args[0]) {
+    help(socket);
+  } else if (!args || 0 == args.length) {
+    help(socket);
+  } else {
+    status(args[0], function (err, status) {
+      if (err) { socket.send(err); }
+      else { socket.send(status); }
+    });
+  }
+};
+
+function help (socket) {
+  socket.send("usage: status [-h] <spotlet>");
+}
+
+function status (spotlet, fn) {
+  var cmd = 'cat '+ path.resolve(SPOTLET_INDEX, spotlet) +'/.status';
+  exec(cmd, function (err, stderr, stdout) {
+    fn(err, stderr || stdout);
+  });
+}
+
